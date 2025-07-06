ee4547fcf8710832230eb91d1ae77ec3a26dac00
PJ <napatsungk@gmail.com>
2025-07-06 02:34:53 -0700

    feat: Create bsp header with GPIO and RCC register maps

---
+++ 01_Bare_Metal_Blink/Debug/Src/subdir.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/gpio.c \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c 
+
+C_DEPS += \
+./Src/gpio.d \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d 
+
+OBJS += \
+./Src/gpio.o \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c Src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/gpio.d ./Src/gpio.o ./Src/main.d ./Src/main.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o
+
+.PHONY: clean-Src
+
